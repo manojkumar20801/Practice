397dd10eaa5c3d4694e632e076d44c6cb9491ed0
manojkumar20801 <rajmano9894@gmail.com>
2026-08-06 01:13:49 +0530

    HW

---
+++ Do.java
@@ -0,0 +1,13 @@
+class Do
+{
+	public static void main(String[] args)
+	{
+	int i=1;
+	do
+	{
+	System.out.println("Manoj");
+	i++;
+	}
+	while(i<=5);
+	}
+}
---
+++ Whil.java
@@ -0,0 +1,12 @@
+class Whil
+{
+	public static void main(String[] args)
+	{
+	int i=10;
+	while(i>=5)
+	{
+	System.out.println(i);
+	i--;
+	}
+	}
+}
